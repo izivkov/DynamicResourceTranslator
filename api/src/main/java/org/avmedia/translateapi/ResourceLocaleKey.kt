5f937b1b4d1ed967a8a8d6a93df91c03973c19b7
Ivo Zivkov <izivkov@gmail.com>
2025-10-24 12:54:11 -0400

    Distinguish same language but different countries.

--- api/src/main/java/org/avmedia/translateapi/ResourceLocaleKey.kt
+++ api/src/main/java/org/avmedia/translateapi/ResourceLocaleKey.kt
@@ -4,16 +4,15 @@ import java.util.Locale
 
 data class ResourceLocaleKey(val resourceId: Int, val locale: Locale) {
     override fun hashCode(): Int {
-        val asString = "$resourceId.${locale.language.lowercase()}"
-        val asHash = asString.hashCode()
-        return asHash
+        val asString = "$resourceId.${locale.language.lowercase()}.${locale.country.uppercase()}"
+        return asString.hashCode()
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is ResourceLocaleKey) return false
         return resourceId == other.resourceId &&
-                locale.language.equals(other.locale.language, ignoreCase = true)
+                locale.language.equals(other.locale.language, ignoreCase = true) &&
+                locale.country.equals(other.locale.country, ignoreCase = true)
     }
 }
-
