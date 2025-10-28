b04a48a5ff3f5bd8bd5296160d320a95742ade39
Ivo Zivkov <izivkov@gmail.com>
2025-10-28 09:32:27 -0400

    Added contry component to Locale

--- app/src/main/java/org/avmedia/dynamicResourceTranslator/MainActivity.kt
+++ app/src/main/java/org/avmedia/dynamicResourceTranslator/MainActivity.kt
@@ -31,10 +31,10 @@ class MainActivity : ComponentActivity() {
         // .addEngine(UppercaseTranslationEngine())
         .setOverwrites(
             arrayOf(
-                ResourceLocaleKey(R.string.hello, Locale("de", "DE")) to { "Aprikose" },
-                ResourceLocaleKey(R.string.hello, Locale("de", "AT")) to { "Marille" },
                 ResourceLocaleKey(R.string.hello, Locale("es")) to { "[Hola] %1\$s" },
                 ResourceLocaleKey(R.string.hello, Locale("bg")) to { "Здравей %1\$s" },
+                ResourceLocaleKey(R.string.hello, Locale("de", "DE")) to { "Aprikose" },
+                ResourceLocaleKey(R.string.hello, Locale("de", "AT")) to { "Marille" },
 
                 ResourceLocaleKey(R.string.hello, Locale("en")) to {
                     val currentHour = java.time.LocalTime.now().hour
